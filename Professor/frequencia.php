486f3605cc2f41dc3a224285fb6ea84dcaee3d56
guilhermejacobi <guilhermevictorjacobi@gmail.com>
2020-11-29 20:28:28 -0300

    layout responsivo part.1

--- Professor/frequencia.php
+++ Professor/frequencia.php
@@ -4,6 +4,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <script type="text/javascript" src="../Admin/JS/mask.js"></script>
     <script type="text/javascript" src="../Admin/JS/chamada/buscaProf.js"></script>
